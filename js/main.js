539835cf632ae6136e3b90962381589179e9b7f9
Barbastark <dorkinacage@gmail.com>
2022-11-06 12:24:03 +0100

    Earth rotation fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-//const surface1 = document.querySelector('.img1');
-//const surface2 = document.querySelector('.img2');
-//const width = surface1.clientWidth;
-//console.log(width)
-//console.log(surface1);
-//setInterval(() => {
-//  let surface1 = document.querySelector('.img1');
-//  console.log(surface1.x)
-//  surface1 = null;
-//}, 100)
+const surface1 = document.querySelector('.img1');
+const surface2 = document.querySelector('.img2');
+const width = surface1.clientWidth;
+console.log(width)
+console.log(surface1);
+setInterval(() => {
+  let surface1 = document.querySelector('.img1');
+  console.log(surface1.x)
+  surface1 = null;
+}, 100)
 
 
