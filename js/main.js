947bebab00a10a5a78980e5c0bc329df2a0d3ffc
Barbastark <dorkinacage@gmail.com>
2022-11-13 12:27:24 +0100

    zoom functionality added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-const surface1 = document.querySelector('.img1');
-const surface2 = document.querySelector('.img2');
-const width = surface1.clientWidth;
-console.log(width)
-console.log(surface1);
-setInterval(() => {
-  let surface1 = document.querySelector('.img1');
-  console.log(surface1.x)
-  surface1 = null;
-}, 100)
+(function() {
+  const root = document.querySelector(":root");
+        root.setAttribute('style', 'font-size:10px;');
+  let fontSize = 10;
+  let min = 1;
+  let max = 55;
+
+  document.onwheel = (e) => {
+    if (fontSize <= min) {
+      fontSize = min + 1;
+    } else if (fontSize >= max) {
+      fontSize = max - 1;
+    } else {
+      fontSize = e.deltaY < 0 ? fontSize += 1 : fontSize -= 1; 
+      root.style.fontSize = `${fontSize}px`;
+    }
+    
+  };
+})();
+
+
 
 
